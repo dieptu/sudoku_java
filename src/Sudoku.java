6edfa3adc247a533375335fda4d4f90ba8128439
tuminhdiep <tuminh2307@gmail.com>
2025-05-01 17:36:30 -0400

    create frame for the game

---
+++ src/Sudoku.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import javax.swing.JFrame;
+
+public class Sudoku {
+    int boardWidth = 600;
+    int boardHeight = 650;
+
+
+    JFrame frame = new JFrame("Sudoku");
+
+    public Sudoku() {
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(new BorderLayout());
+    }
+    
+}
